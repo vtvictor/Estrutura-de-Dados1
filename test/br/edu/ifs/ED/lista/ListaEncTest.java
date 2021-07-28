7dec723c47346271c9e9db3965747c2cc928316b
Victor Carvalho <87211894+vtvictor@users.noreply.github.com>
2021-07-28 17:30:31 -0300

    Delete test/br/edu/ifs/ED directory

--- test/br/edu/ifs/ED/lista/ListaEncTest.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifs.ED.lista;
-
-import org.junit.jupiter.api.BeforeEach;
-
-class ListaEncTest extends ListaTest {
-    @BeforeEach
-    public void setUp() { lista = new ListaEnc<Integer>();
-    }
-}
